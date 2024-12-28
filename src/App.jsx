a1d4feb42f15eb0b77813f55da2f46a335f6dd49
Fred <huertaserick@yahoo.ie>
2024-12-28 20:05:52 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="border-2">test</div>
+    </>
+  );
+}
+
+export default App;
